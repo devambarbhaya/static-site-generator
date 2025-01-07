5bbf5339174c643632a89f8ac4a9692868d7288a
devambarbhaya <barbhaya.devam@gmail.com>
2025-01-07 09:04:58 +0000

    add TextNode

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode, TextType
+
+def main():
+  trial = TextNode("This is a text node", TextType.BOLD_TEXT, "https://www.boot.dev")
+  print(trial.__repr__())
+  
+  
+main()
