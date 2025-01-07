d4ed86df32023c0ccc8d0d129e4190f0af634345
devambarbhaya <barbhaya.devam@gmail.com>
2025-01-07 10:10:57 +0000

    add TextNode Tests

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from textnode import TextNode, TextType
 
 def main():
-  trial = TextNode("This is a text node", TextType.BOLD_TEXT, "https://www.boot.dev")
+  trial = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
   print(trial.__repr__())
   
   
