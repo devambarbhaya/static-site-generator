f5671525ca5b9edb541c26222f0e41aa5f9c1461
devambarbhaya <barbhaya.devam@gmail.com>
2025-01-09 20:31:22 +0000

    add copying functionality from static assets to public assests

--- src/main.py
+++ src/main.py
@@ -1,11 +1,28 @@
+import os
+import shutil
 from text_node import TextNode, TextType
 from html_node import HTMLNode
 
-def main():
-  textnode_trial = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-  print(textnode_trial.__repr__())
+def copy_static(source, destination):
+  if os.path.exists(destination):
+    shutil.rmtree(destination)
+    print(f"Deleted existing destination: {destination}")
+    
+  os.makedirs(destination)
+  print(f"Created directory: {destination}")
   
-  htmlnode_trial = HTMLNode("p", "paragraph", [], {})
-  print(htmlnode_trial.__repr__())
+  for item in os.listdir(source):
+    source_item = os.path.join(source, item)
+    destination_item = os.path.join(destination, item)
+    
+    if os.path.isfile(source_item):
+      shutil.copy(source_item, destination_item)
+      print(f"Copied file: {source_item} -> {destination_item}")
+    elif os.path.isdir(source_item):
+      shutil.copytree(source_item, destination_item)
+      print(f"Copied directory: {source_item} -> {destination_item}")
+
+def main():
+  copy_static("static", "public")
   
 main()
